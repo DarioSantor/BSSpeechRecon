d9a8c82cb90158b075393c8c91c175b67fe0ea03
Dario Ferreira Santos <darsantos@deloitte.pt>
2023-07-13 11:51:38 +0100

    initial commit

--- Sources/BSSpeechRecon/BSSpeechRecon.swift
+++ Sources/BSSpeechRecon/BSSpeechRecon.swift
@@ -1,6 +1,109 @@
-public struct BSSpeechRecon {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import Speech
+import Combine
 
-    public init() {
+public class JDSpeechRecon: NSObject, SFSpeechRecognizerDelegate {
+    var silenceTimer: Timer?
+    var cancellables = Set<AnyCancellable>()
+    
+    private let speechRecognizer: SFSpeechRecognizer = {
+        if let recognizer = SFSpeechRecognizer(locale: Locale(identifier: Locale.current.identifier)),
+            recognizer.isAvailable {
+            return recognizer
+        } else {
+            let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))!
+            print("Speech recognizer is not available for the current locale. Using American English")
+            return speechRecognizer
+        }
+    }()
+    
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+    
+    var stopSignal = PassthroughSubject<Void, Never>()
+    
+    override init() {
+        super.init()
+        speechRecognizer.delegate = self
+
+        stopSignal
+            .sink { _ in
+                self.audioEngine.stop()
+                self.recognitionRequest?.endAudio()
+                print("Listening has stopped!")
+            }.store(in: &cancellables)
+    }
+    
+    public func getSpeechRecognitionPermission() -> PassthroughSubject<SFSpeechRecognizerAuthorizationStatus, Never> {
+        let permissionSubject: PassthroughSubject<SFSpeechRecognizerAuthorizationStatus, Never> = .init()
+        
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            permissionSubject.send(authStatus)
+        }
+        
+        return permissionSubject
+    }
+    
+    private func resetSilenceTimer() {
+        silenceTimer?.invalidate()
+        silenceTimer = Timer.scheduledTimer(withTimeInterval: 5, repeats: false) { [weak self] _ in
+            self?.stopSignal.send()
+        }
+    }
+
+    public func startRecording() -> AnyPublisher<String, Never> {
+        print("Listening has started!")
+        let textSpeechSubject = PassthroughSubject<String, Never>()
+
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        recognitionRequest!.shouldReportPartialResults = true
+
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest!) { [weak self] result, error in
+            guard let self = self else { return }
+            var isFinal = false
+
+            if let result = result {
+                textSpeechSubject.send(result.bestTranscription.formattedString)
+                isFinal = result.isFinal
+                self.resetSilenceTimer()
+            }
+
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                self.audioEngine.inputNode.removeTap(onBus: 0)
+
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+            }
+        }
+
+        let recordingFormat = audioEngine.inputNode.outputFormat(forBus: 0)
+        audioEngine.inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { [weak self] buffer, _ in
+            guard let self = self else { return }
+            self.recognitionRequest?.append(buffer)
+        }
+
+        audioEngine.prepare()
+
+        do {
+            try audioEngine.start()
+        } catch {
+            print("Audio engine failed to start: \(error.localizedDescription)")
+        }
+
+        let silencePublisher = textSpeechSubject
+            .handleEvents(receiveSubscription: { _ in
+                self.resetSilenceTimer()
+            }, receiveCompletion: { _ in
+                self.silenceTimer?.invalidate()
+                self.silenceTimer = nil
+            }, receiveCancel: {
+                self.silenceTimer?.invalidate()
+                self.silenceTimer = nil
+            })
+
+        return silencePublisher
+            .eraseToAnyPublisher()
     }
 }
--- Tests/BSSpeechReconTests/BSSpeechReconTests.swift
+++ Tests/BSSpeechReconTests/BSSpeechReconTests.swift
@@ -1,11 +1,11 @@
 import XCTest
 @testable import BSSpeechRecon
 
-final class BSSpeechReconTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(BSSpeechRecon().text, "Hello, World!")
-    }
-}
+//final class BSSpeechReconTests: XCTestCase {
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct
+//        // results.
+//        XCTAssertEqual(BSSpeechRecon().text, "Hello, World!")
+//    }
+//}
