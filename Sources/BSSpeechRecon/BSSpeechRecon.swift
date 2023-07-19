7d78372dbca95033bb6146ec167ff9ed55e025bd
Dario Ferreira Santos <darsantos@deloitte.pt>
2023-07-19 15:33:55 +0100

    remove print statments

--- Sources/BSSpeechRecon/BSSpeechRecon.swift
+++ Sources/BSSpeechRecon/BSSpeechRecon.swift
@@ -8,7 +8,7 @@ public class BSSpeechRecon: NSObject, SFSpeechRecognizerDelegate {
     
     private let speechRecognizer: SFSpeechRecognizer = {
         if let recognizer = SFSpeechRecognizer(locale: Locale(identifier: Locale.current.identifier)),
-            recognizer.isAvailable {
+           recognizer.isAvailable {
             return recognizer
         } else {
             let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))!
@@ -27,12 +27,11 @@ public class BSSpeechRecon: NSObject, SFSpeechRecognizerDelegate {
     public override init() {
         super.init()
         speechRecognizer.delegate = self
-
+        
         stopSignal
             .sink { _ in
                 self.audioEngine.stop()
                 self.recognitionRequest?.endAudio()
-                print("Listening has stopped!")
             }.store(in: &cancellables)
     }
     
@@ -52,45 +51,44 @@ public class BSSpeechRecon: NSObject, SFSpeechRecognizerDelegate {
     /// - Parameter shutDownTimer: Seconds that the user can be quit without stopping the service.
     /// - Returns: A Combine publisher that holds the user speech transcribed in a string.
     public func startListening(_ shutDownTimer: Int = 3) -> AnyPublisher<String, Never> {
-        print("Listening has started!")
         let textSpeechSubject = PassthroughSubject<String, Never>()
-
+        
         recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
         recognitionRequest!.shouldReportPartialResults = true
-
+        
         recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest!) { [weak self] result, error in
             guard let self = self else { return }
             var isFinal = false
-
+            
             if let result = result {
                 textSpeechSubject.send(result.bestTranscription.formattedString)
                 isFinal = result.isFinal
-                    self.resetSilenceTimer(shutDownTimer: shutDownTimer != 0 ? shutDownTimer : 300)
+                self.resetSilenceTimer(shutDownTimer: shutDownTimer != 0 ? shutDownTimer : 300)
             }
-
+            
             if error != nil || isFinal {
                 self.audioEngine.stop()
                 self.audioEngine.inputNode.removeTap(onBus: 0)
-
+                
                 self.recognitionRequest = nil
                 self.recognitionTask = nil
             }
         }
-
+        
         let recordingFormat = audioEngine.inputNode.outputFormat(forBus: 0)
         audioEngine.inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { [weak self] buffer, _ in
             guard let self = self else { return }
             self.recognitionRequest?.append(buffer)
         }
-
+        
         audioEngine.prepare()
-
+        
         do {
             try audioEngine.start()
         } catch {
             print("Audio engine failed to start: \(error.localizedDescription)")
         }
-
+        
         let silencePublisher = textSpeechSubject
             .handleEvents(receiveSubscription: { _ in
                 self.resetSilenceTimer(shutDownTimer: shutDownTimer)
@@ -101,7 +99,7 @@ public class BSSpeechRecon: NSObject, SFSpeechRecognizerDelegate {
                 self.silenceTimer?.invalidate()
                 self.silenceTimer = nil
             })
-
+        
         return silencePublisher
             .eraseToAnyPublisher()
     }
